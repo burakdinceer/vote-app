7337aa14d0786bc9983908a017ad580bc5b8b790
burakdinceeer@gmail.com <burakdinceeer@gmail.com>
2023-03-02 20:30:45 +0300

    reduce vote increase vote

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,48 +1,66 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Data from '../../Data/Data.json'
 
 const List = () => {
-    const [value,setValue] = useState(0)
-    const [selectedItemsUp, setSelectedItemsUp] = useState({});
-    const [selectedItemsDown, setSelectedItemsDown] = useState({});
+    
+    const [data,setData] = useState([])
+    const upVoteHandle = (e) => {
+      const obj = data.find(item => e === item.id)
+      obj.vote ++
 
-    const upVoteHandle = (item) => {
-        setSelectedItemsUp((prevSelectedItems) => ({
-            ...prevSelectedItems,
-            [item.id]: !prevSelectedItems[item.id],
-          }));
+      setData(
+        data.map(item =>{ 
+          if(item.id ===e) {
+            return {
+              ...item,
+              obj
+            }
+          }
+          return item
+        } )
+      )
+    }
+   
 
+    useEffect(() => {
+        setData(Data)
+    },[])
+    
+    const downVoteHandle = (e) => {
+      const obj = data.find(item => e === item.id)
+      obj.vote --
+
+      setData(
+        data.map(item =>{ 
+          if(item.id ===e) {
+            return {
+              ...item,
+              obj
+            }
+          }
+          return item
+        } )
+      )
     }
-    const downVoteHandle = (item) => {
-        setSelectedItemsDown((prevSelectedItems) => ({
-            ...prevSelectedItems,
-            [item.id]: !prevSelectedItems[item.id],
-          }));
-          
-        
-    }
-    const art = () => {
-        setValue(value+1)
-    }
-    console.log(selectedItemsUp)
+    
   return (
     <div>
         
       {
-        Data.map(itm => (
+        data.map(itm => (
            <div>
             
              <div key={itm.id} className='flex justify-center items-center'>
                 
                 <div className='w-24 h-24 border-x border-y flex justify-center items-center mt-2 rounded-md border-gray-400 text-2xl bg-gray-300 '>
-                {value}
+                {itm.vote}
                     Points</div>
                 <div className='ml-3'>
                    <div className='font-bold text-2xl'> {itm.name}</div>
                    <div className='opacity-70'>  {itm.url}</div>
                     <div className='text-sm opacity-60 mt-4'>
-                        <button onClick={()=>upVoteHandle(itm)} className='mr-20 '> {selectedItemsUp[itm.id] ? "Not Selected" : "Selected"}</button>
-                        <button onClick={()=>downVoteHandle(itm)}>{selectedItemsDown[itm.id] ? "Not Selected" : "Selected"}</button>
+                        <button onClick={()=>upVoteHandle(itm.id)} className='mr-20 '>+Vote</button>
+                        <button disabled={itm.vote === 0} onClick={()=>downVoteHandle(itm.id)}>-Vote</button>
                     </div>
                 </div>
             </div>
