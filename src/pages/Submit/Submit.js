0f1d355c803c40f36647a206b154b9bbb3470b4f
burakdinceeer@gmail.com <burakdinceeer@gmail.com>
2023-03-08 22:22:16 +0300

    submit comments

--- src/pages/Submit/Submit.js
+++ src/pages/Submit/Submit.js
@@ -1,35 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header/Header'
 import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { addListItem } from '../../redux/commentsSlice'
+import { Form } from 'react-bootstrap'
+import uuid from 'react-uuid';
 
 const Submit = () => {
     
-    
+  const [formData,setFormData] = useState({ name: '', id: uuid(), url: '',upvote:5 })
    const dispatch = useDispatch()
-
-   const handlePage = () => {
-    dispatch(addListItem())
-   }
+   console.log(formData)
+   
+   const handleSubmit = (event) => {
+    console.log(event )
+    event.preventDefault();
+    dispatch(addListItem(formData))
+    
+  };
+   const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
   return (
     <div>
        <div> <Header/></div>
+       <Form onSubmit={handleSubmit}>
        <div className='flex flex-col items-center '>
        <Link className='text-sm font-bold mr-48'  to={'/'}> {'<-'}Return to List</Link>
         <h2 className='font-bold text-3xl mr-24  mt-5'>Add New Link</h2>
          <div className='m-5'>
             <p className='text-m'>Link Name</p>
-           <input placeholder='Name.....' className='border-gray-400 border-x border-y w-72 h-8 rounded-sm'/>
+           <input type='text' name='name' onChange={handleChange} placeholder='Name.....' className='border-gray-400 border-x border-y w-72 h-8 rounded-sm'/>
          </div>
          <div>
          <p className='text-m'>Link Url</p>
-           <input placeholder='Url.....' className='border-gray-400 border-x border-y w-72 h-8 rounded-sm'/>
+           <input type='text' name='email' onChange={handleChange} placeholder='Url.....' className='border-gray-400 border-x border-y w-72 h-8 rounded-sm'/>
         </div>
         
         
-        <button onClick={()=>handlePage()} className='w-28 h-12 bg-black text-white mt-5 rounded-lg font-extrabold'>ADD</button>
+        <button type='submit' className='w-28 h-12 bg-black text-white mt-5 rounded-lg font-extrabold'>ADD</button>
        </div>
+       </Form>
     </div>
   )
 }
